7bc2ff570a8e6aaa252880d981369be3d828d268
dport2469 <54684346+dport2469@users.noreply.github.com>
2024-09-17 11:50:40 -1000

    xxx

---
+++ Lab.Lists/Ex1.py
@@ -0,0 +1,21 @@
+first_name = input("First name:")
+middle_name = input("Middle name:")
+last_name = input("Last name:")
+
+# Ex1a
+#full_name = first_name + " " + middle_name[0] + ". " + last_name
+
+# Ex1b
+#full_name = f"{first_name} {middle_name[0]}. {last_name}"
+
+# Ex1c
+#full_name = "%s %s. %s"%(first_name, middle_name[0], last_name)
+
+#Ex1d
+#full_name = "{} {}. {}".format(first_name, middle_name[0], last_name)
+
+#Ex1c
+
+# full_name = "".join([first_name," ", middle_name[0],". ",last_name])
+
+print(full_name)
